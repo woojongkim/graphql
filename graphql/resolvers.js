4e6b87ac4b159bf5cfb30ccd1ccb8fc7314c90c8
woody <5385kim@gmail.com>
2020-11-28 17:17:10 +0900

    #11 add/delete graphql function added

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,14 @@
-import { people, getById } from "./db";
+import { getMovies, getById, deleteMovie, addMovie } from "./db";
 
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => getById(id),
+    Movies: () => getMovies(),
+    Movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { title, pic, summary, score }) =>
+      addMovie(title, pic, summary, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
